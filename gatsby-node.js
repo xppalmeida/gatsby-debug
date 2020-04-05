2b691a334a469c909db6673e534833d3af1e1aa5
Pedro Paulo Almeida Silva <pedropauloalmeida@gmail.com>
2020-04-05 20:44:14 +0100

    chore: improve console.log of errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,3 @@
-function debug(...args) {
-  args.forEach(arg => {
-    console.log('+++++++++++++++++++++++++++++++++', '\n', JSON.stringify(arg, null, 2), '\n');
-  });
-}
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     query {
@@ -21,17 +16,19 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panic('failed to create pages', result.errors);
   }
 
-  debug(JSON.stringify(result, null, 2));
+  console.log('result', JSON.stringify(result, null, 2));
   const pages = result.data.allMdx.nodes;
 
   pages.forEach(page => {
-    debug('+++++++++++++++++++++++++++++++++', page.frontmatter.slug);
-    actions.createPage({
+    console.log('+++++++++++++++++++++++++++++++++ slug: ', page.frontmatter.slug);
+    const config = {
       path: page.frontmatter.slug,
       component: require.resolve(`./${page.frontmatter.component}`),
       context: {
         slug: page.frontmatter.slug,
       },
-    });
+    };
+    console.log('config to create page', JSON.stringify(config, null, 2));
+    actions.createPage(config);
   });
 };
