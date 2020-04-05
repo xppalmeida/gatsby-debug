e6d87c8a723dea5f15f2d11bd48ff597ef813f83
Pedro Paulo Almeida Silva <pedropauloalmeida@gmail.com>
2020-04-05 20:09:52 +0100

    chore: rendering from MDX files but error in graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,8 @@
+function debug(...args) {
+  args.forEach(arg => {
+    console.log('+++++++++++++++++++++++++++++++++', '\n', JSON.stringify(arg, null, 2), '\n');
+  });
+}
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const result = await graphql(`
     query {
@@ -16,10 +21,11 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panic('failed to create pages', result.errors);
   }
 
+  debug(JSON.stringify(result, null, 2));
   const pages = result.data.allMdx.nodes;
 
   pages.forEach(page => {
-    console.log('page', page.frontmatter);
+    debug('+++++++++++++++++++++++++++++++++', page.frontmatter.slug);
     actions.createPage({
       path: page.frontmatter.slug,
       component: require.resolve(`./${page.frontmatter.component}`),
